4da07ae757b87c6692f1c8cdbb49747e70314329
Felix Hu <hhu@vmware.com>
2013-07-22 18:46:41 +0800

    call set_field in vcap-common to ignore unknown field
    Change-Id: Ia4858e01f94c38642e39029e7a1ce028cb54356b

--- lib/base/api/message.rb
+++ lib/base/api/message.rb
@@ -7,16 +7,11 @@ require 'base'
 class ServiceMessage < JsonMessage
 
   def set_field(field, value)
-    field = field.to_sym
-    raise ValidationError.new({field => "Unknown field #{field}"}) unless self.class.fields.has_key?(field)
-    f = self.class.fields[field]
-    # delete an optional field
-    if value.nil? and f.required == false
+    f = self.class.fields[field.to_sym]
+    if value.nil? && f && f.required == false
       @msg.delete(field)
     else
-      errs = f.schema.validate(value)
-      raise ValidationError.new({field => errs}) if errs
-      @msg[field] = value
+      super(field, value)
     end
   end
 
