6f015441c77a3f6de64c9c96a802c872956aebfe
Harshawardhan Gadgil <hgadgil@rbcon.com>
2013-10-18 21:10:15 -0700

    Filter out services not maintained by this gateway
    [Fixes: #59104116]
    
    Change-Id: I7a15fa178d9deb6110d596c6b1faead0cdb45c98

--- spec/function_test/catalog_manager_scv1_spec.rb
+++ spec/function_test/catalog_manager_scv1_spec.rb
@@ -306,5 +306,163 @@ module VCAP::Services::ServicesControllerClient
       end.should have_been_made
 
     end
+
+    it 'filters out and updates only the services offered by this gateway' do
+      unique_service_id = 'unique_service_id'
+      unique_plan_id = 'unique_plan_id'
+      cc_service_guid = 'service-guid'
+      cc_service_plan_guid = 'cc_service_plan_guid'
+      service_label = 'id-1.0'
+      service_provider = 'provider'
+      timeout_secs = 30
+      free_plan_extra_new = "new_props"
+
+      updated_catalog =
+          {
+              "id_provider" =>
+               {
+                   "description" => 'description',
+                   "provider" => 'provider',
+                   "version" => '1.0',
+                   "url" => 'url',
+                   "plans" => {
+                       'free' => {
+                           description: 'description',
+                           free: true,
+                           extra: free_plan_extra_new,
+                           unique_id: unique_plan_id,
+                       },
+                   },
+                   "unique_id" => unique_service_id,
+                   "label" => 'id-1',
+                   "active" => true,
+                   "acls" => 'acls',
+                   "timeout" => timeout_secs,
+                   "extra" => 'extra'
+               }
+          }
+      stub_request(:get, "http://api.vcap.me/api/v1/services").
+          to_return(:status => 200, :body => Yajl::Encoder.encode(
+          {
+              "total_results" => 2,
+              "total_pages" => 1,
+              "prev_url" => nil,
+              "next_url" => nil,
+              "resources" => [
+                  {
+                      'metadata' => {
+                          'guid' => cc_service_guid,
+                          "url" => "http://api.vcap.me/api/v1/services/92a06847-9446-407f-8aaa-a6f20c1d2cdc",
+                          "created_at" => "2013-10-16 18:20:54 UTC",
+                          "updated_at" => "2013-10-16 18:25:33 UTC"
+                      },
+                      'entity' => {
+                          'id' => unique_service_id,
+                          'label' => service_label,
+                          'version' => '1.0',
+                          'active' => true,
+                          "url" => 'url',
+                          'timeout_secs' => timeout_secs,
+                          'provider' => service_provider,
+                          'description' => 'description',
+                          'properties' => nil,
+                      }
+                  },
+                  {
+                      'metadata' => {
+                          'guid' => "some_other_cc_service_guid",
+                          "url" => "http://api.vcap.me/api/v1/services/92a06847-9446-407f-8aaa-a6f20c1d2abc",
+                          "created_at" => "2013-10-16 18:20:54 UTC",
+                          "updated_at" => "2013-10-16 18:25:33 UTC"
+                      },
+                      'entity' => {
+                          'id' => "some_other_unique_service_id",
+                          'label' => "some_other_service_label",
+                          'version' => '1.0',
+                          'active' => true,
+                          "url" => 'url',
+                          'timeout_secs' => timeout_secs,
+                          'provider' => service_provider,
+                          'description' => 'description',
+                          'properties' => nil,
+                      }
+                  },
+              ]
+          }))
+
+      stub_request(:put, "http://api.vcap.me/api/v1/services/#{cc_service_guid}").
+          to_return(:body => Yajl::Encoder.encode('metadata' => {'guid' => cc_service_guid}))
+
+      stub_request(:get, "http://api.vcap.me/api/v1/service_plans?service_id=#{cc_service_guid}").
+          to_return(:status => 200, :body => Yajl::Encoder.encode(
+          {
+              "total_results" => 1,
+              "total_pages" => 1,
+              "prev_url" => nil,
+              "next_url" => nil,
+              "resources" => [
+                  {
+                      'metadata' => {
+                          'guid' => cc_service_plan_guid,
+                          "url" => "http://api.vcap.me/api/v1/service_plans/#{cc_service_plan_guid}",
+                          "created_at" => "2013-10-16 18:20:54 UTC",
+                          "updated_at" => "2013-10-16 18:25:33 UTC"
+                      },
+                      'entity' => {
+                          'id' => unique_plan_id,
+                          'service_id' => unique_service_id,
+                          'name' => 'free',
+                          'version' => "1",
+                          'description' => "description",
+                          'properties' => "{\"free\":true,\"extra\":null,\"public\":true}"
+                      }
+                  },
+              ]
+          }))
+      stub_request(:get, "http://api.vcap.me/api/v1/service_plans?service_id=some_other_cc_service_guid").
+          to_return(:status => 200, :body => Yajl::Encoder.encode(
+          {
+              "total_results" => 1,
+              "total_pages" => 1,
+              "prev_url" => nil,
+              "next_url" => nil,
+              "resources" => [
+                  {
+                      'metadata' => {
+                          'guid' => cc_service_plan_guid,
+                          "url" => "http://api.vcap.me/api/v1/service_plans/#{cc_service_plan_guid}",
+                          "created_at" => "2013-10-16 18:20:54 UTC",
+                          "updated_at" => "2013-10-16 18:25:33 UTC"
+                      },
+                      'entity' => {
+                          'id' => unique_plan_id,
+                          'service_id' => unique_service_id,
+                          'name' => 'free',
+                          'version' => "1",
+                          'description' => "description",
+                          'properties' => "{\"free\":true,\"extra\":null,\"public\":true}"
+                      }
+                  },
+              ]
+          }))
+
+      stub_request(:put, "http://api.vcap.me/api/v1/service_plans/cc_service_plan_guid").
+          to_return(:status => 200, :body => Yajl::Encoder.encode('metadata' => {'guid' => cc_service_plan_guid}))
+
+      EM.run do
+        catalog_manager.update_catalog(true, -> { updated_catalog })
+        EM.add_timer(1) { EM.stop }
+      end
+
+      update_requests_count = 0
+      a_request(:put, "http://api.vcap.me/api/v1/services/#{cc_service_guid}").with do |req|
+        JSON.parse(req.body).fetch('id').should == unique_service_id
+        update_requests_count = update_requests_count + 1
+        true
+      end.should have_been_made
+
+      update_requests_count.should == 1
+
+    end
   end
 end
