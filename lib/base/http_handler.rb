a81ea563727c65ee44d7a40a85b6bee6947363ee
Andrew Liu <aliu@vmware.com>
2013-10-21 19:28:45 +0800

    Fix error
    Change-Id: I9038b62a02dba65f198ad300352c2e7f83e48467

--- lib/base/http_handler.rb
+++ lib/base/http_handler.rb
@@ -31,7 +31,7 @@ class HTTPHandler
 
   def regenerate_http_header
     @cc_req_hdrs = {'Content-Type' => 'application/json'}
-    @cc_req_hdrs << {'Authorization' => @auth_token_generator.call } if @auth_token_generator
+    @cc_req_hdrs.merge!({'Authorization' => @auth_token_generator.call }) if @auth_token_generator
   end
 
 
