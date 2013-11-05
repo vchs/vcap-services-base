761f9ac18bc622fd71f25a0cdce3a6ccae12af0f
Eagle Chen <yanguangchen@vmware.com>
2013-11-06 02:22:01 +0800

    refactor backup job and add some common methods
    Change-Id: I5ca07c2e062027c4a5ee7339462677714baa19f5

--- lib/base/job/backup.rb
+++ lib/base/job/backup.rb
@@ -3,15 +3,13 @@ require "fileutils"
 require "vcap/logging"
 require "fog"
 
-require_relative "../service_error"
-require_relative "./package.rb"
+require_relative "./base"
 
 module VCAP::Services::Base::AsyncJob
   module Backup
-    include VCAP::Services::Base::Error
 
     LOCAL_BACKUP_PATH = "/tmp".freeze
-    FILTER_KEYS = %w(backup_id date).freeze
+    FILTER_KEYS = %w(backup_id size date).freeze
 
     def fmt_time()
       # UTC time in ISO 8601 format.
@@ -137,71 +135,23 @@ module VCAP::Services::Base::AsyncJob
     end
 
     # common utils for backup job
-    class BackupJob
-      attr_reader :name, :backup_id
+    class BackupJob < BaseJob
+      attr_reader :backup_id
 
       include Backup
-      include Resque::Plugins::Status
-      extend  JobPatch
-
-      class << self
-        def queue_lookup_key
-          :node_id
-        end
-
-        def select_queue(*args)
-          result = nil
-          args.each do |arg|
-            result = arg[queue_lookup_key] if arg.is_a?(Hash) && arg.has_key?(queue_lookup_key)
-          end
-          @logger = Config.logger
-          @logger.info("Select queue #{result} for job #{self.class} with args:#{args.inspect}") if @logger
-          result
-        end
-      end
 
       def initialize(*args)
         super(*args)
-        parse_config
-        init_worker_logger
         DBClient.logger = Config.logger
         StorageClient.logger = Config.logger
       end
 
-      def fmt_error(e)
-        "#{e}: [#{e.backtrace.join(" | ")}]"
-      end
-
-      def init_worker_logger
-        @logger = Config.logger
-      end
-
-      def required_options(*args)
-        missing_opts = args.select{|arg| !options.has_key? arg.to_s}
-        raise ArgumentError, "Missing #{missing_opts.join(', ')} in options: #{options.inspect}" unless missing_opts.empty?
-      end
-
-      def create_lock
-        lock_name = "lock:backup:#{name}"
-        ttl = @config['job_ttl'] || 600
-        lock = Lock.new(lock_name, :logger => @logger, :ttl => ttl)
-        lock
+      def lock_key
+        "lock:backup:#{name}"
       end
 
       def get_dump_path
         LOCAL_BACKUP_PATH
-      end
-
-      def parse_config
-        @config = Yajl::Parser.parse(ENV['WORKER_CONFIG'])
-        raise "Need environment variable: WORKER_CONFIG" unless @config
-      end
-
-      def handle_error(e)
-        @logger.error("Error in #{self.class} uuid:#{@uuid}: #{fmt_error(e)}")
-        err = (e.instance_of?(ServiceError)? e : ServiceError.new(ServiceError::INTERNAL_ERROR)).to_hash
-        err_msg = Yajl::Encoder.encode(err["msg"])
-        failed(err_msg)
       end
     end
   end
